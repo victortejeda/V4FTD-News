468e47cdaf654e3ab070846ce8f9197e8eb36125
victortejeda <losmejoresxxx28@gmail.com>
2022-08-12 21:33:00 -0400

    Initial Commit

---
+++ V4FTD News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  V4FTD News
+//
+//  Created by Victor Tejeda on 12/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ V4FTD News/V4FTD_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  V4FTD_NewsApp.swift
+//  V4FTD News
+//
+//  Created by Victor Tejeda on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct V4FTD_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
